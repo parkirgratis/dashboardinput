2f53e64db5c93fc3ebbf245962c63ce728a07fae
rayfanaqbil <rayfana09@gmail.com>
2024-11-07 15:17:30 +0700

    add sweetalert js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,24 +7,3 @@ import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 if (getCookie("login")===""){
     redirect("/signin");
 }
-
-//logic logout
-const logoutButton = document.getElementById("logoutButton");
-
-if (logoutButton) {
-  logoutButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    Cookies.remove("login", {
-      path: "/",
-    });
-    Swal.fire({
-      icon: "success",
-      title: "Logged Out",
-      text: "You have been successfully logged out",
-      showConfirmButton: false,
-      timer: 2000,
-    }).then(() => {
-      window.location.href = "/";
-    });
-  });
-}
